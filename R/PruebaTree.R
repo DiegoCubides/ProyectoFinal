b7a8ed4df430e3816ad25ace22b28dac13d879db
t0w4s <tomasf.mendez@ecci.edu.co>
2023-05-28 13:04:00 -0500

    Avance

--- R/PruebaTree.R
+++ R/PruebaTree.R
@@ -1,2 +1,20 @@
 library(rpart)
-summary(cu.summary)
+library(tidyverse)
+library(caret)
+
+folder <-  dirname(rstudioapi::getSourceEditorContext()$path )
+box_test <-read_csv(paste0(folder,"/dataset_ambiente.csv"))
+
+
+head(box_test)
+
+dummy <- dummyVars(" ~ AMBIENTE",data = AMBIENTE.box_test)
+
+fit <- rpart(
+  AMBIENTE ~ ROJO + VERDE + AZUL,
+  method = "anova",
+  data = box_test
+)
+
+plot(fit, uniform = TRUE, margin = 0.1)
+text(fit, use.n = TRUE,all = TRUE, cex=.8)
