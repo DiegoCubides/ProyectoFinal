3ac962f7b858691d21f18f16c720a40d5393fff8
DiegoCubides <juand.cubidesor@ecci.edu.co>
2023-05-28 17:33:50 -0500

    Knn

--- R/KNN.R
+++ R/KNN.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(caret)
+
 folder <-  dirname(rstudioapi::getSourceEditorContext()$path )
 dataset <-read_csv(paste0(folder,"/Dataset_ambiente.csv"))
-datasetcopia <-read_csv(paste0(folder,"/Dataset_ambiente.csv"))
+datasetcopia <-read_csv(paste0(folder,"/Dataset_ambiente2.csv"))
 head(dataset)
 hist(dataset$ROJO,breaks= 10)
 hist(dataset$VERDE,breaks= 10)
@@ -10,8 +11,10 @@ hist(dataset$AZUL,breaks= 10)
 hist(dataset$TEMP,breaks= 10)
 hist(dataset$HUME,breaks= 10)
 hist(dataset$PT100,breaks= 10)
+kable(summary(dataset))
 
 dataset$AMBIENTE <-as.factor(dataset$AMBIENTE)
+datasetcopia$AMBIENTE <- as.factor(datasetcopia$AMBIENTE)
 
 plot(dataset[1:5]
      ,pch=21,bg=c("green","blue3","yellow")[unclass(dataset$AMBIENTE)])
@@ -32,7 +35,8 @@ Knnfit <- train(AMBIENTE ~ ROJO+VERDE+AZUL+TEMP+HUME+PT100
                 ,method = "knn", trControl = ctrl
                 ,preProcess= c("range") #Range
                 ,tuneLength=20)
-Knnpredict <- predict(Knnfit,newdata = test.data)
+Knnpredict <- predict(Knnfit,newdata = datasetcopia)
 confusionMatrix(Knnpredict
-                ,test.data$AMBIENTE)
-summary(Knnfi)
+                ,datasetcopia$AMBIENTE)
+
+
