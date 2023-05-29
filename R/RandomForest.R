3418fc93d44afcfa6b46d84014e6226ffb10b2b1
t0w4s <tomasf.mendez@ecci.edu.co>
2023-05-28 21:18:07 -0500

    cambio FIT

--- R/RandomForest.R
+++ R/RandomForest.R
@@ -10,9 +10,8 @@ data.samples <- sample(1:nrow(dataset.complete),
                        nrow(dataset.complete)*0.7, replace = FALSE)
 training.data <- dataset.complete[data.samples, ]
 test.data <- dataset.complete[-data.samples, ]
-fit.rf <- randomForest(AMBIENTE ~ ROJO+VERDE+AZUL+TEMP+HUME+PT100
-                       ,method = "class"
-                       ,data=dataset)
+fit.rf <- randomForest(AMBIENTE ~ ROJO  +VERDE + AZUL + TEMP + HUME +
+                         PT100, data = training.data)
 prediction.rf <- predict(fit.rf, test.data)
 output <- data.frame(test.data$AMBIENTE, prediction.rf)
 RMSE= sqrt(sum((output$test.data.AMBIENTE -output$prediction.rf)^2))/
